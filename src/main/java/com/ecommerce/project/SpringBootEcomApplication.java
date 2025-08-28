d36eebf5e68ad46b10c8812a11b3d5b71c696dc7
zhiminV <zhimin@ualberta.ca>
2025-08-28 15:50:29 -0700

    First commit: Spring Boot Ecommerce project

---
+++ src/main/java/com/ecommerce/project/SpringBootEcomApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcomApplication.class, args);
+	}
+
+}
