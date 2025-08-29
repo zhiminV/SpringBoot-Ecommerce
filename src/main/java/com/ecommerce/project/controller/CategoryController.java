00ccf8d259c0241e1f9484c4f76026cfd570179a
zhiminV <zhimin@ualberta.ca>
2025-08-28 17:42:56 -0700

    add POST endpoint to create categories

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,6 +2,8 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -15,5 +17,12 @@ public class CategoryController {
     public List<Category> getAllCategories()
     {
         return categories;
+    }
+
+    @PostMapping("/api/public/categories")
+    public String createCategory(@RequestBody Category category)
+    {
+        categories.add(category);
+        return "Category created successfully";
     }
 }
