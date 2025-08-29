dc38a284d8ca9f7205436a5ac6765282ded3d100
zhiminV <zhimin@ualberta.ca>
2025-08-28 17:36:54 -0700

    add Category model and controller

---
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -0,0 +1,22 @@
+package com.ecommerce.project.model;
+
+public class Category {
+    private Long categoryId;
+    private String categoryName;
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
