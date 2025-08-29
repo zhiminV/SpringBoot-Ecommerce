569bf1572870d16849ac7fe0075b81e259e44245
zhiminV <zhimin@ualberta.ca>
2025-08-28 22:30:16 -0700

    introduce CategoryService layer and update controller

---
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -0,0 +1,9 @@
+package com.ecommerce.project.service;
+import com.ecommerce.project.model.Category;
+import java.util.List;
+
+// use loose coupling
+public interface CategoryService {
+    List<Category> getAllCategories();
+    void addCategory(Category category);
+}
