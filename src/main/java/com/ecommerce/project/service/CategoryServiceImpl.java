bf1e7e6da090f886907c1aef56ae8e400c45dcf5
Zhimin <122182731+zhiminV@users.noreply.github.com>
2025-08-29 22:25:21 -0700

    add ResponseEntity to deleteCategory

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,7 +1,10 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,10 +32,9 @@ public class CategoryServiceImpl implements CategoryService {
         //first find the category that need to delete
         Category category = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst().orElse(null);
-        if (category == null) {
-            return "Category not found";
-        }
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
         categories.remove(category);
         return "Category with " + categoryId + " deleted successfully";
     }
