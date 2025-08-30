b6a61f25e2e6ba386174e17b69b0fcb4f0e314e5
Zhimin <122182731+zhiminV@users.noreply.github.com>
2025-08-30 10:29:02 -0700

    add IDENTITY generation strategy and constructors to Category entity

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,13 +1,24 @@
 package com.ecommerce.project.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity(name = "Categories")
 public class Category {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
+
+    public Category(Long categoryId, String categoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+    public Category() {
+
+    }
 
     public Long getCategoryId() {
         return categoryId;
