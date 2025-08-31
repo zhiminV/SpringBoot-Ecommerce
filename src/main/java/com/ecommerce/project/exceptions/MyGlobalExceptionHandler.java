d18955e20db7d479d447615873640fe762cd6ee6
Zhimin <122182731+zhiminV@users.noreply.github.com>
2025-08-31 11:13:02 -0700

    add global exception handler for validation errors

---
+++ src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.ecommerce.project.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class MyGlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    //ResponseEntity will response the http status , if not valid will pass 400 , not 200ok
+    public ResponseEntity<Map<String, String>> myMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        //  Map<String, String>:  key is the field, and value is error
+        Map<String, String> response = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(err -> {
+            //  getField() -> get all the error, like CategoryName, price...
+            String fieldName = ((FieldError)err).getField();
+            // message to client:  "must not be blank"
+            String message = err.getDefaultMessage();
+            response.put(fieldName,message);
+        });
+        // 400 Bad request
+        return new ResponseEntity<Map<String,String>>(response,
+                HttpStatus.BAD_REQUEST);
+    }
+}
