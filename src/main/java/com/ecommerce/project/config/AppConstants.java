3b9e56eb8296a3b88029d08f154250dd9350c1c0
Zhimin <122182731+zhiminV@users.noreply.github.com>
2025-09-01 20:17:30 -0700

    add default value for page

---
+++ src/main/java/com/ecommerce/project/config/AppConstants.java
@@ -0,0 +1,8 @@
+package com.ecommerce.project.config;
+
+import com.ecommerce.project.payload.CategoryResponse;
+
+public class AppConstants {
+    public static final String PAGE_NUMBER = "0";
+    public static final String PAGE_SIZE = "50";
+}
--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.controller;
 
+import com.ecommerce.project.config.AppConstants;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
@@ -29,8 +30,8 @@ public class CategoryController {
 //    @GetMapping("/api/public/categories")
     @RequestMapping(value ="/public/categories" , method = RequestMethod.GET )
     public ResponseEntity<CategoryResponse> getAllCategories(
-            @RequestParam(name="pageNumber") Integer pageNumber,
-            @RequestParam(name="pageSize") Integer pageSize
+            @RequestParam(name="pageNumber",defaultValue = AppConstants.PAGE_NUMBER) Integer pageNumber,
+            @RequestParam(name="pageSize",defaultValue = AppConstants.PAGE_SIZE) Integer pageSize
     )
     {
         CategoryResponse CategoryResponse = categoryService.getAllCategories(pageNumber, pageSize);
