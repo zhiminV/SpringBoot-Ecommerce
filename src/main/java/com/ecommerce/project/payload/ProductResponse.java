3fdd8eec9d96cd5ab48981df2ec1ef453590d5d1
Zhimin <122182731+zhiminV@users.noreply.github.com>
2025-09-05 07:36:23 -0700

    initial setup of product module

---
+++ src/main/java/com/ecommerce/project/payload/ProductResponse.java
@@ -0,0 +1,16 @@
+package com.ecommerce.project.payload;
+
+
+import com.ecommerce.project.model.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductResponse {
+    private List<ProductDTO> content;
+}
