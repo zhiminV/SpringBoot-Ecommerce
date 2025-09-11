fef81228688d336f13bc631dfb7acbd6e1ba0e47
Zhimin <122182731+zhiminV@users.noreply.github.com>
2025-09-11 09:08:48 -0700

    Managing Security Config

--- src/main/java/com/ecommerce/project/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/ecommerce/project/security/service/UserDetailsServiceImpl.java
@@ -3,14 +3,15 @@ package com.ecommerce.project.security.service;
 import com.ecommerce.project.model.User;
 import com.ecommerce.project.repositories.UserRepository;
 import jakarta.transaction.Transactional;
-import lombok.Setter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 
-@Setter
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
