fcc577cad961d7684f0f22939576f5bf00c7ea41
Zhimin <122182731+zhiminV@users.noreply.github.com>
2025-09-11 22:16:37 -0700

    Add Shopping Cart model

---
+++ src/main/java/com/ecommerce/project/model/CartItem.java
@@ -0,0 +1,33 @@
+package com.ecommerce.project.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@Table(name = "cart_item")
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartItemId;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private int quantity;
+    private double discount;
+    private double productPrice;
+
+}
