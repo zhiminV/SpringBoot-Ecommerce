55545c3f77651ad8e4595521e58a5edb5ee734f8
Zhimin <122182731+zhiminV@users.noreply.github.com>
2025-09-14 21:43:06 -0700

    add OrderController with endpoint to place orders

---
+++ src/main/java/com/ecommerce/project/controller/OrderController.java
@@ -0,0 +1,39 @@
+package com.ecommerce.project.controller;
+
+import com.ecommerce.project.model.OrderItem;
+import com.ecommerce.project.payload.OrderDTO;
+import com.ecommerce.project.payload.OrderRequestDTO;
+import com.ecommerce.project.repositories.OrderItemRepository;
+import com.ecommerce.project.util.AuthUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    AuthUtil authUtil;
+
+    @PostMapping("/order/users/payments/{paymentMethod}")
+    public ResponseEntity<OrderDTO> orderProducts(@PathVariable String paymentMethod,
+                                                  @RequestBody OrderRequestDTO orderRequestDTO) {
+        String emailId = authUtil.loggedInEmail();
+        OrderDTO order = orderService.placeOrder(
+                emailId,
+                orderRequestDTO.getAddressId(),
+                paymentMethod,
+                orderRequestDTO.getPgName(),
+                orderRequestDTO.getPaymentId(),
+                orderRequestDTO.getPgStatus(),
+                orderRequestDTO.getPgResponseMessage()
+        );
+        return new ResponseEntity<>(order, HttpStatus.CREATED);
+
+    }
+}
