a6d5fc46aa12cbe4d664f06ad0e66942c4ccfda3
Zhimin <122182731+zhiminV@users.noreply.github.com>
2025-09-14 21:10:00 -0700

    add Payment entity and link to Order

---
+++ src/main/java/com/ecommerce/project/model/Payment.java
@@ -0,0 +1,41 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="payments")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+
+    @OneToOne(mappedBy = "payment",cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
+    private Order order;
+
+    @NotBlank
+    @Size(min = 4, message = "Payment method must contain at least 4 characters")
+    private String paymentMethod;
+
+    private String pgPaymentId;
+    private String pgStatus;
+    private String pgResponseMessage;
+    private String pgName;
+
+    public  Payment(Long paymentId,String pgPaymentId,String pgStatus,String pgResponseMessage,String pgName) {
+        this.paymentId =  paymentId;
+        this.pgPaymentId = pgPaymentId;
+        this.pgStatus = pgStatus;
+        this.pgResponseMessage = pgResponseMessage;
+        this.pgName = pgName;
+    }
+
+}
